adf1e91544744c0f6111a4a0190a6d59529b7945
Margaux GERARD <margauxgerard@MacBook-Pro-de-Margaux.local>
2018-04-26 17:58:13 +0200

    Main modifiéé

--- main.c
+++ main.c
@@ -9,3 +9,64 @@ int main()
 	printf("Coucou les zamis\n");
     return 0;
 }
+
+// Initialisations
+struct fractal buffer = (struct fractal)malloc(sizeof(struct fractal) * maxthreads);
+struct fractal fract_table [1000];
+pthread_mutex_t mutex;
+sem_t empty; // Sert a compter le nombre de slots qui sont vides dans le buffer partagé
+sem_t full; // Sert a compter le nombre de slots qui sont remplis dans le buffer partagé
+pthread_mutex_init(&mutex,NULL);
+sem_init(&empty,maxthreads); // Initialisé à la longueur du buffer
+sem_init(&full,0); // Initialisé à 0
+
+// Threads de lecture
+void producteur (){
+    char lecture;
+    while (true){
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+        // Section critique
+        reader();
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+    }
+}
+
+// Threads de calcul
+void consommateur (){
+    long calcul;
+    while (true){
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        // Section critique
+        calculer();
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+    }
+}
+
+// Lecture
+void reader (){
+    int fd = open(0,O_RDONLY);
+    if (fd == -1){
+        return -1;
+    }
+    char * buff;
+    int rd = read(fd, buf, sizeof(char));
+    for (int i = 0; rd != -1 && buffer[i] != '\n'; i++) {
+        rd = read(fd, buffer, sizeof(char));
+    }
+    if (buffer[0]=='#' || buffer==NULL){
+        break;
+    }
+    char * tab = strtok(buffer, ' '); // Split la ligne qui a été lue
+    while (tab != NULL){
+        tab = strtok(NULL, ' ');
+    }
+}
+
+// Calcul
+void calculer (){
+    
+}
